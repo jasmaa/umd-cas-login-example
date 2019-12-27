8477e8507938138c5c29815dda00c42fc786d638
jasmaa <jasonmaa3955@gmail.com>
2019-12-27 10:44:42 -0500

    Fix url bug

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const BASE_URL = 'https://example.com'; // Your app's url
 app.get('/login', (req, res) => {
 
     if (req.query.ticket) {
-        axios.get(`https://shib.idm.umd.edu/shibboleth-idp/profile/cas/serviceValidate?ticket=${req.query.ticket}&service=${BASE_URL}`)
+        axios.get(`https://shib.idm.umd.edu/shibboleth-idp/profile/cas/serviceValidate?ticket=${req.query.ticket}&service=${BASE_URL}/login`)
             .then(response => {
                 res.send(response.data); // Process directory id here
             })
@@ -17,7 +17,7 @@ app.get('/login', (req, res) => {
             });
     }
     else {
-        res.redirect(`https://shib.idm.umd.edu/shibboleth-idp/profile/cas/login?service=${BASE_URL}`);
+        res.redirect(`https://shib.idm.umd.edu/shibboleth-idp/profile/cas/login?service=${BASE_URL}/login`);
     }
 });
 
